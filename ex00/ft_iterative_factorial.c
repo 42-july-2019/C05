e58eafe7e1d16e3718046ffe825e632600e8d8c7
Nathanael LABREUIL <alabreui@e2r11p16.42.fr>
2019-07-11 17:17:11 +0200

    Fix ex00 to account for negative argument

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,7 +6,7 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 16:39:04 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/11 16:47:31 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/11 17:15:14 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@ int		ft_iterative_factorial(int nb)
 	int result;
 	int i;
 
+	if (nb < 0)
+		return (0);
 	i = 1;
 	result = 1;
 	while (i <= nb)
