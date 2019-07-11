e0923e8300dcaab5f0af9d1b7630716031ca3a4b
Nathanael LABREUIL <alabreui@e2r11p16.42.fr>
2019-07-11 17:31:26 +0200

    Fix norm for ex01

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -6,7 +6,7 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/11 17:17:36 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/11 17:23:47 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/11 17:31:11 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int		ft_recursive_factorial(int nb)
 		result = nb * ft_recursive_factorial(nb - 1);
 	else
 		result = 1;
-	return result;
+	return (result);
 }
