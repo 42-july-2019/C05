be1b94bf72701433e0b748a479c1577ec13ef903
Nathanael LABREUIL <alabreui@e2r11p16.42.fr>
2019-07-11 17:50:02 +0200

    Add ex03 & Improve ex01 exception check

--- ex03/ft_recursive_power.c
+++ ex03/ft_recursive_power.c
@@ -1,26 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/11 17:30:23 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/11 17:48:22 by alabreui         ###   ########.fr       */
+/*   Created: 2019/07/11 17:44:04 by alabreui          #+#    #+#             */
+/*   Updated: 2019/07/11 17:47:22 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int		ft_iterative_power(int nb,int power)
+int	ft_recursive_power(int nb, int power)
 {
 	int result;
-
-	result = 1;
+	
 	if (nb <= 0 || power < 0)
-		return 0;
-	while (power > 0)
-	{
-		result = result * nb;
-		power--;
-	}
-	return result;
+		return (0);
+	else if (power > 0)
+		result = nb * ft_recursive_power(nb, power - 1);
+	else
+		result = 1;
+	return (result);
 }
