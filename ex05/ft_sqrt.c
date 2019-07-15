ae6d3528754c7f77dc4d310289ce21f031cac47c
Nathanael Labreuil <alabreui@student.42.fr>
2019-07-15 13:42:32 +0200

    Fix overflow problem in ex05

--- ex05/ft_sqrt.c
+++ ex05/ft_sqrt.c
@@ -6,20 +6,22 @@
 /*   By: alabreui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/12 11:42:11 by alabreui          #+#    #+#             */
-/*   Updated: 2019/07/14 22:17:41 by alabreui         ###   ########.fr       */
+/*   Updated: 2019/07/15 13:38:13 by alabreui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_sqrt(int nb)
 {
-	int	result;
+	long int	result;
+	long int	nbr;
 
 	result = 1;
+	nbr = nb;
 	if (nb >= 100000000)
 		result = 10000;
-	while (result * result < nb)
+	while (result * result < nbr)
 		result++;
-	if (nb % result == 0)
+	if (nbr % result == 0)
 		return (result);
 	return (0);
 }
